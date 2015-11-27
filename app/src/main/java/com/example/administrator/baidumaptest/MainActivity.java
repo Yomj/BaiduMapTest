dc8634c778c6c58576b9e5ff5327f74a2bfdb1ce
Yomj <yaomiaoju@gmail.com>
2015-11-27 16:04:11 +0800

    测试了定位的功能

--- app/src/main/java/com/example/administrator/baidumaptest/MainActivity.java
+++ app/src/main/java/com/example/administrator/baidumaptest/MainActivity.java
@@ -1,23 +1,81 @@
 package com.example.administrator.baidumaptest;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.baidu.mapapi.BMapManager;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BaiduMapOptions;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView;
+    private BaiduMap baiduMap;
+    LocationClient locationClient;
+    boolean isFirstLocated = true;
+    public MyLocationListener myLocationListener = new MyLocationListener();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mapView = (MapView) findViewById(R.id.map_view);
+        SDKInitializer.initialize(getApplicationContext());
+        mapView = new MapView(this,new BaiduMapOptions());
+        setContentView(mapView);
+        baiduMap = mapView.getMap();
+        baiduMap.setMyLocationEnabled(true);
+        locationClient = new LocationClient(this);
+        locationClient.registerLocationListener(myLocationListener);
+    }
 
+    public class MyLocationListener implements BDLocationListener{
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            if(bdLocation == null || mapView == null){
+                return;
+            }
+            MyLocationData myLocationData = new MyLocationData.Builder().accuracy(bdLocation.getRadius()).direction(100).latitude(bdLocation.getLatitude()).longitude(bdLocation.getLongitude()).build();
+            baiduMap.setMyLocationData(myLocationData);
+            if(isFirstLocated){
+                isFirstLocated = false;
+                LatLng latLng = new LatLng(bdLocation.getLatitude(),bdLocation.getLongitude());
+                MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newLatLng(latLng);
+                baiduMap.animateMapStatus(mapStatusUpdate);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationClient.stop();
+        baiduMap.setMyLocationEnabled(true);
+        mapView.onDestroy();
+        mapView = null;
     }
 
     @Override
